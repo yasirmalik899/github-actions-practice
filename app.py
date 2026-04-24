e22ea8ab9796de1671c8a21fa49122ce4fc5fb96
Ubuntu <azureuser@Devops.330k3odhrr1uxce0n2sxs5rvsc.gx.internal.cloudapp.net>
2026-04-24 10:29:58 +0000

    lsuu

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 print("Hello from Docker container")
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "GitHub Actions Capstone App is running!"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
